9753a87eac029c966657b38e2778bfb4d0be6d5e
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-03-09 15:04:26 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+
+const app = express();
+
+
+
+// middlewate
+app.use(morgan('tiny'));
+
+
+
+
+app.get('/', (req, resp) => {
+    resp.send('Welcome to Festival Finder!');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    debug(`Running on port ${port}`)
+});
