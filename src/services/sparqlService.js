f5024c0d3340e3d54dbb3dca7957d5f607a43bf3
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-03-11 16:50:54 +0100

    Sparql service update

--- src/services/sparqlService.js
+++ src/services/sparqlService.js
@@ -8,34 +8,45 @@ function sparqlService() {
     let parsedQuery = null;
 
     const searchFestivals = async (term) => {
-        parsedQuery = parser.parse(
 
-            'PREFIX foaf: <http://xmlns.com/foaf/0.1/> '
-            + 'PREFIX dbr: <http://dbpedia.org/resource/> '
-            + 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> '
-            + 'PREFIX dbo: <http://dbpedia.org/ontology/> '
-            + 'PREFIX dbp: <http://dbpedia.org/property/> '
-            + 'SELECT ?festivali, ?topic, ?zanr, ?dati, ?sleden, ?websajt, ?posleden_pat, ?slika, ?lokacija '
-            + 'FROM <https://dbpedia.org/> '
-            + 'WHERE { '
-            + 'dbr:List_of_music_festivals_in_South_Korea dbo:wikiPageWikiLink ?festivali. '
-            + '?festivali rdfs:label ?topic. '
-            + '?festivali dbp:genre ?zanr. #mozebi treba dbo:genre '
-            + 'optional {?festivali dbp:dates ?dati}. '
-            + 'optional {?festivali dbp:next ?sleden}. '
-            + 'optional {?festivali dbo:wikiPageExternalLink ?websajt}. '
-            + 'optional {?festivali dbp:last ?posleden_pat}. '
-            + 'optional {?festivali dbo:thumbnail ?slika} '
-            + 'optional {?festivali dbp:location ?lokacija} '
-            + 'filter(lang(?topic)="en")'
-        );
+        debug('AJMO');
 
-        const generator = new SparqlGenerator({});
-        parsedQuery.variables = ['?festivali'];
-        const generatedQuery = generator.stringify(parsedQuery);
+        try {
+            parsedQuery = await parser.parse(
 
-        debug('-----------GENERIRANO QUERY:');
-        debug(generatedQuery);
+                'PREFIX foaf: <http://xmlns.com/foaf/0.1/> '
+                + 'PREFIX dbr: <http://dbpedia.org/resource/> '
+                + 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> '
+                + 'PREFIX dbo: <http://dbpedia.org/ontology/> '
+                + 'PREFIX dbp: <http://dbpedia.org/property/> '
+                + 'SELECT ?festivali ?topic ?zanr ?dati ?sleden ?websajt ?posleden_pat ?slika ?lokacija '
+                + 'FROM <https://dbpedia.org/> '
+                + 'WHERE { '
+                + 'dbr:List_of_music_festivals_in_South_Korea dbo:wikiPageWikiLink ?festivali. '
+                + '?festivali rdfs:label ?topic. '
+                + '?festivali dbp:genre ?zanr. #mozebi treba dbo:genre '
+                + 'optional {?festivali dbp:dates ?dati}. '
+                + 'optional {?festivali dbp:next ?sleden}. '
+                + 'optional {?festivali dbo:wikiPageExternalLink ?websajt}. '
+                + 'optional {?festivali dbp:last ?posleden_pat}. '
+                + 'optional {?festivali dbo:thumbnail ?slika}. '
+                + 'optional {?festivali dbp:location ?lokacija}. '
+                + 'filter(lang(?topic)="en"). '
+                + '}'
+            );
+    
+            const generator = new SparqlGenerator({});
+            parsedQuery.variables = ['?festivali'];
+            const generatedQuery = generator.stringify(parsedQuery);
+    
+            debug('-----------GENERIRANO QUERY:');
+            debug(generatedQuery);
+
+        } catch (err) {
+            debug(err);
+        }
+
+        
     };
 
 
